7d5d7b6b5006612cb0200416f0685fc0559a0954
Francisco Contreras <fcocc77@gmail.com>
2020-10-23 15:27:09 -0300

    instalador: init

---
+++ resources/app/vinacomp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /opt/vinacomp
+dir=$(pwd)
+
+./bin/vinacomp "$@"
---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+install_dir='/opt/vinacomp'
+
+function install() {
+    # Compilacion
+    sh scripts/build.sh
+
+    mkdir ./bin
+    cp gui/release/vinacomp ./bin/vinacomp
+    #
+    #
+
+    # Copiado a la carpeta de instalacion
+    mkdir $install_dir
+
+    cp -rf ./bin $install_dir
+    cp -rf ./config $install_dir
+    cp -rf ./resources $install_dir
+
+    cp ./resources/app/vinacomp.sh $install_dir/vinacomp
+    cp ./resources/app/VinaComp.desktop /usr/local/share/applications/VinaComp.desktop
+    #
+    #
+}
+
+function uninstall() {
+    rm -rf $install_dir
+    rm /usr/local/share/applications/VinaComp.desktop
+}
+
+uninstall
+install
