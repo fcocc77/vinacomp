86cc7ff0db005537904e083388fcbc375fe8289d
Francisco Contreras <fcocc77@gmail.com>
2020-11-18 19:44:56 -0300

    curve view: iconos

--- gui/src/curve_view_popup.cpp
+++ gui/src/curve_view_popup.cpp
@@ -6,7 +6,7 @@ void curve_view::popup_setup_ui()
 
     // Edit : Select All Key Frames
     QAction *select_all_action = new QAction("Select All Keyframes");
-    select_all_action->setIcon(QIcon("resources/images/properties_a.png"));
+    select_all_action->setIcon(QIcon("resources/images/select_all_a.png"));
     select_all_action->setShortcut(QString("Ctrl+A"));
     connect(select_all_action, &QAction::triggered, this, &curve_view::select_all_key_frames);
     popup->addAction(select_all_action);
@@ -15,7 +15,7 @@ void curve_view::popup_setup_ui()
 
     // Edit : Delete Key Frames
     QAction *delete_keyframes_action = new QAction("Delete Keyframes");
-    delete_keyframes_action->setIcon(QIcon("resources/images/properties_a.png"));
+    delete_keyframes_action->setIcon(QIcon("resources/images/close_a.png"));
     delete_keyframes_action->setShortcut(QString("Backspace"));
     popup->addAction(delete_keyframes_action);
     //
@@ -25,7 +25,7 @@ void curve_view::popup_setup_ui()
 
     // Interpolation_menu : Linear
     QAction *linear_action = new QAction("Linear Interpolation");
-    linear_action->setIcon(QIcon("resources/images/properties_a.png"));
+    linear_action->setIcon(QIcon("resources/images/linear_a.png"));
     linear_action->setShortcut(QString("L"));
     connect(linear_action, &QAction::triggered, this, [this]() {
     });
@@ -67,7 +67,7 @@ void curve_view::popup_setup_ui()
 
     // View : Center To Curve
     QAction *center_action = new QAction("Center To Curve");
-    center_action->setIcon(QIcon("resources/images/properties_a.png"));
+    center_action->setIcon(QIcon("resources/images/fit_curve_a.png"));
     center_action->setShortcut(QString("F"));
     connect(center_action, &QAction::triggered, this, &curve_view::fit_viewport_to_keyframes);
     popup->addAction(center_action);
