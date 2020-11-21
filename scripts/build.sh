8f09da517a621e67deb262e9bf657f0cce731591
Francisco Contreras <fcocc77@gmail.com>
2020-11-21 12:45:42 -0300

    ...

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@ pkill -9 vinacomp
 
 npm run d
 rm gui/Makefile
+rm gui/release/vinacomp
 
 # cambia 'path' del proyecto para el archivo 'style.css'
 css='resources/css/style.css'
@@ -18,4 +19,4 @@ sh scripts/svg_converter.sh
 qmake-qt5
 make -j 4
 
-optirun gui/release/vinacomp
+gui/release/vinacomp
