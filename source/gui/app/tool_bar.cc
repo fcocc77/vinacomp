5e00568533fe2843b294b0495b78e6ace12f83e8
Francisco Contreras <fcocc77@gmail.com>
2020-11-22 15:59:58 -0300

    tool bar: ...

--- source/gui/app/tool_bar.cc
+++ source/gui/app/tool_bar.cc
@@ -1,50 +1,23 @@
 #include <tool_bar.h>
 
 tool_bar::tool_bar(maker *__maker)
-    : _maker(__maker)
+    : _maker(__maker),
+      open_menus(false)
 {
+    this->setMouseTracking(true);
+    this->setTabletTracking(true);
 
     this->setObjectName("tool_bar");
     this->setMaximumWidth(40);
 
-    layout = new QVBoxLayout();
+    layout = new QVBoxLayout(this);
     layout->setContentsMargins(2, 10, 2, 10);
-    this->setLayout(layout);
 
     setup_ui();
 }
 
 tool_bar::~tool_bar()
 {
-}
-
-void tool_bar::add_menu(QString group, QString icon_group)
-{
-    int icon_size = 25;
-
-    QMenu *menu = new QMenu(this);
-    for (QJsonValue value : _maker->get_effects(group))
-    {
-        QJsonObject effect = value.toObject();
-        QString label = effect["label"].toString();
-        QString id = effect["id"].toString();
-        QString icon = effect["icon"].toString();
-
-        QAction *effect_action = new QAction(label);
-        effect_action->setIcon(QIcon("resources/images/" + icon + ".png"));
-        menu->addAction(effect_action);
-
-        connect(effect_action, &QAction::triggered, this, [=]() {
-            _maker->create_fx(id);
-        });
-    }
-    QPushButton *popup_button = new QPushButton();
-    connect(popup_button, &QPushButton::clicked, this, [=]() {
-        menu->popup(QCursor::pos());
-        menu->show();
-    });
-    qt::set_icon(popup_button, icon_group, icon_size);
-    layout->addWidget(popup_button);
 }
 
 void tool_bar::setup_ui()
@@ -71,4 +44,80 @@ void tool_bar::setup_ui()
     QPushButton *comp_layout = new QPushButton();
     qt::set_icon(comp_layout, "vertical_split_a", icon_size);
     layout->addWidget(comp_layout);
+}
+
+void tool_bar::add_menu(QString group, QString icon_group)
+{
+    int icon_size = 25;
+
+    QPushButton *popup_button = new QPushButton(this);
+    popup_button->setMouseTracking(true);
+    popup_button->setTabletTracking(true);
+
+    popup_button->setObjectName(icon_group);
+
+    menu *_menu = new menu(popup_button);
+    menus.insert(icon_group, _menu);
+
+    connect(popup_button, &QPushButton::clicked, this, [=] {
+        open_menus = true;
+
+        _menu->exec(popup_button->mapToGlobal({this->width(), 0}));
+        this->setFocus();
+    });
+
+    for (QJsonValue value : _maker->get_effects(group))
+    {
+        QJsonObject effect = value.toObject();
+        QString label = effect["label"].toString();
+        QString id = effect["id"].toString();
+        QString icon = effect["icon"].toString();
+
+        QAction *effect_action = new QAction(label);
+        effect_action->setIcon(QIcon("resources/images/" + icon + ".png"));
+        _menu->addAction(effect_action);
+
+        connect(effect_action, &QAction::triggered, this, [=]() {
+            _maker->create_fx(id);
+            open_menus = false;
+            this->setFocusPolicy(Qt::NoFocus);
+        });
+    }
+
+    qt::set_icon(popup_button, icon_group, icon_size);
+
+    layout->addWidget(popup_button);
+}
+
+void tool_bar::focusOutEvent(QFocusEvent *event)
+{
+    open_menus = false;
+}
+
+void tool_bar::mouseMoveEvent(QMouseEvent *event)
+{
+    if (open_menus)
+    {
+        QWidget *widget = qApp->widgetAt(QCursor::pos());
+        QPushButton *button = dynamic_cast<QPushButton *>(widget);
+        if (button)
+        {
+            menu *_menu = menus.value(button->objectName());
+            if (_menu)
+            {
+                _menu->exec(button->mapToGlobal({this->width(), 0}));
+                this->setFocus();
+                open_menus = true;
+            }
+        }
+    }
+}
+
+void menu::mouseMoveEvent(QMouseEvent *e)
+{
+    if (e->pos().x() < 0)
+        if (e->pos().y() < 0 || e->pos().y() > button->height())
+            this->hide();
+
+    QMenu::mouseMoveEvent(e);
 }
--- source/gui/app/tool_bar.h
+++ source/gui/app/tool_bar.h
@@ -7,15 +7,33 @@
 #include <QPushButton>
 #include <QMenu>
 #include <QJsonObject>
+#include <QLabel>
+#include <QApplication>
 
 #include <qt.h>
 #include <maker.h>
+#include <util.h>
+
+class menu : public QMenu
+{
+private:
+    QWidget *button;
+
+public:
+    menu(QWidget *_button) : QMenu(_button), button(_button){};
+    ~menu(){};
+
+protected:
+    void mouseMoveEvent(QMouseEvent *e) override;
+};
 
 class tool_bar : public QWidget
 {
 private:
     QVBoxLayout *layout;
     maker *_maker;
+    bool open_menus;
+    QMap<QString, menu *> menus;
 
     void setup_ui();
     void add_menu(QString group, QString icon_group);
@@ -23,6 +41,10 @@ private:
 public:
     tool_bar(maker *_maker);
     ~tool_bar();
+
+protected:
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void focusOutEvent(QFocusEvent *event) override;
 };
 
 #endif // TOOL_BAR_HPP
