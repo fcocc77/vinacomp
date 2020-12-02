bb806924dcf48be505fd01fcfa55d22d848b101c
Francisco Contreras <fcocc77@gmail.com>
2020-12-02 10:24:53 -0300

    ...

--- source/gui/viewer/time_line_paint.cc
+++ source/gui/viewer/time_line_paint.cc
@@ -21,8 +21,8 @@ void time_line::draw_in_out()
     draw_line({input, 1000000}, {input, -1000000}, red, 2);
     draw_line({output, 1000000}, {output, -1000000}, red, 2);
 
-    draw_box({{first_frame, -100000}, {input, 100000}}, QColor({70, 0, 0}));
-    draw_box({{output, -100000}, {last_frame, 100000}}, QColor({70, 0, 0}));
+    draw_box({{first_frame, -100000}, {input, 100000}}, QColor({20, 20, 20}));
+    draw_box({{output, -100000}, {last_frame, 100000}}, QColor({20, 20, 20}));
     draw_box({{input, -100000}, {output, 100000}}, QColor({30, 30, 30}));
 
     float input_x = get_position({input, 0}).x();
