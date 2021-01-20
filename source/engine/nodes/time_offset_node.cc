fa6b912ba50420287490213acb7eeec8664ce0e3
pancho <pancho@localhost.localdomain>
2021-01-20 18:20:22 -0300

    time_offset_node: reverse parametro

--- source/engine/nodes/time_offset_node.cc
+++ source/engine/nodes/time_offset_node.cc
@@ -13,5 +13,6 @@ time_offset_node::~time_offset_node()
 void time_offset_node::set_offset(QJsonObject *params, int &frame)
 {
 	int offset = params->value("time_offset").toArray()[0].toInt();
+	bool reverse = params->value("reverse_input").toBool();
 	frame -= offset;
 }
