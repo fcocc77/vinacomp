07e2138f173f5a1256adbb818ce19f5f326dd2dc
pancho <pancho@localhost.localdomain>
2021-02-11 23:49:45 -0300

    setup.sh : update ...

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,12 @@
-
 sudo yum -y install \
 	opencv-devel \
-	inkscape
+	inkscape \
+	qt5-qtbase \
+	qt5-qtbase-devel \
+    mesa-libGL-devel \
+    mesa-libGLU-devel \
+    qt5-qtmultimedia.x86_64 \
+    qt5-qtmultimedia-devel.x86_64 \
+
 
 sudo pip3 install -r requirements.txt
-
-
-
