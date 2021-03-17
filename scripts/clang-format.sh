c25526318fadf18e0ebc09cbf28baa1def1ef116
Francisco <fcocc77@gmail.com>
2021-03-17 18:30:38 -0300

    ...

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -3,7 +3,6 @@ source /opt/rh/llvm-toolset-7.0/enable
 
 # da formato a todos los archivos .cc y .h de la carpeta 'source'.
 # el archivo de cofiguracion de clang-format es '.clang-format'
-# clang-format -i -style=file source/**/*.h
 clang-format -i -style=file source/*/*.cc source/*/*.h
 clang-format -i -style=file source/*/*/*.cc source/*/*/*.h
 
