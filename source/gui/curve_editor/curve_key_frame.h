a10e1b96e5f88c9f242b4a12c257c4443092d497
Francisco <fcocc77@gmail.com>
2021-04-05 15:05:40 -0400

    curve_key_frame: con inline...

--- source/gui/curve_editor/curve_key_frame.h
+++ source/gui/curve_editor/curve_key_frame.h
@@ -27,34 +27,147 @@ public:
     key_frame(QString _curve_name, int _index, QPointF _position, QColor _color);
     ~key_frame();
 
-    QPointF pos();
-    float x();
-    float y();
-    void set_pos(QPointF _position);
+    inline QPointF pos();
+    inline float x();
+    inline float y();
+    inline void set_pos(QPointF _position);
 
-    float get_left_angle();
-    float get_right_angle();
+    inline float get_left_angle();
+    inline float get_right_angle();
 
-    void set_left_angle(float _angle);
-    void set_right_angle(float _angle);
+    inline void set_left_angle(float _angle);
+    inline void set_right_angle(float _angle);
 
-    bool selected();
-    bool is_broken();
-    bool set_broken(bool _broken);
+    inline bool selected();
+    inline bool is_broken();
+    inline bool set_broken(bool _broken);
     void set_interpolation(int left_number, int right_number);
-    int right_interpolation();
-    int left_interpolation();
+    inline int right_interpolation();
+    inline int left_interpolation();
 
-    QColor get_color();
+    inline QColor get_color();
 
-    void select(bool select);
-    int get_index();
-    QString get_curve();
-    float exaggeration();
-    void set_exaggeration(float _exaggeration);
-    void stamp_position();
-    void restore_position();
-    QPointF get_last_position();
+    inline void select(bool select);
+    inline int get_index();
+    inline QString get_curve();
+    inline float exaggeration();
+    inline void set_exaggeration(float _exaggeration);
+    inline void stamp_position();
+    inline void restore_position();
+    inline QPointF get_last_position();
 };
+
+inline QPointF key_frame::pos()
+{
+    return position;
+}
+
+inline void key_frame::stamp_position()
+{
+    // guarda una posicion anterior que es
+    // independiente de la posicion actual
+    last_position = position;
+}
+
+inline void key_frame::restore_position()
+{
+    // restaura la posicion guardada en 'last_position'
+    position = last_position;
+}
+
+inline QPointF key_frame::get_last_position()
+{
+    return last_position;
+}
+
+inline float key_frame::x()
+{
+    return position.x();
+}
+
+inline float key_frame::y()
+{
+    return position.y();
+}
+
+inline void key_frame::set_pos(QPointF _position)
+{
+    position = _position;
+}
+
+inline float key_frame::get_left_angle()
+{
+    return left_angle;
+}
+
+inline float key_frame::get_right_angle()
+{
+    return right_angle;
+}
+
+inline void key_frame::set_left_angle(float _angle)
+{
+    left_angle = _angle;
+}
+
+inline void key_frame::set_right_angle(float _angle)
+{
+    right_angle = _angle;
+}
+
+inline bool key_frame::selected()
+{
+    return _selected;
+}
+
+inline void key_frame::select(bool select)
+{
+    _selected = select;
+}
+
+inline int key_frame::get_index()
+{
+    return index;
+}
+
+inline QString key_frame::get_curve()
+{
+    return curve_name;
+}
+
+inline float key_frame::exaggeration()
+{
+    return _exaggeration;
+}
+
+inline void key_frame::set_exaggeration(float __exaggeration)
+{
+    _exaggeration = __exaggeration;
+}
+
+inline bool key_frame::is_broken()
+{
+    return broken;
+}
+
+inline bool key_frame::set_broken(bool _broken)
+{
+    broken = _broken;
+}
+
+inline int key_frame::right_interpolation()
+{
+    return _right_interpolation;
+}
+
+inline int key_frame::left_interpolation()
+{
+    return _left_interpolation;
+}
+
+inline QColor key_frame::get_color()
+{
+    return color;
+}
 
 #endif // CURVE_KEY_FRAME_HPP
