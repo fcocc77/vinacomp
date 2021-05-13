02bf432c3ef6f122c1230ea23596e3a8ec7d5a02
pancho <pancho@localhost.localdomain>
2021-05-13 15:02:31 -0400

    knob_editor_utils: correccion en params file dialog

--- source/gui/properties/knob_editor_utils.cc
+++ source/gui/properties/knob_editor_utils.cc
@@ -103,7 +103,7 @@ knob_params knob_editor::get_params_from_edit_box(QWidget *panel) const
         _allowed_file_types.push_back(_file_type.simplified());
 
     params.allowed_file_types = _allowed_file_types;
-    save_file_dialog_check->is_checked();
+    params.save_file_dialog = save_file_dialog_check->is_checked();
 
     return params;
 }
