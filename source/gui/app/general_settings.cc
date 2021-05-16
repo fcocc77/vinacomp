41be5d79fb19f5ee324f40108758bb72e0f7a9e1
pancho <pancho@localhost.localdomain>
2021-05-16 19:40:22 -0400

    general_settings: save and resotre settings

--- source/gui/app/general_settings.cc
+++ source/gui/app/general_settings.cc
@@ -1,8 +1,10 @@
 #include <QSplitter>
 
 #include <general_settings.h>
+#include <global.h>
 #include <knob_check_box.h>
 #include <knob_integer.h>
+#include <util.h>
 
 general_settings::general_settings(QWidget *_vinacomp)
     : settings(true, _vinacomp)
@@ -31,12 +33,21 @@ void general_settings::setup_auto_save()
         new knob_check_box({}, "Enable AutoSave", true);
     knob_integer *delay_knob = new knob_integer({}, 1, 30, 5);
 
+    connect(enable_knob, &knob_check_box::changed, this,
+            [this](bool checked) { data.insert("auto_save_enable", checked); });
+
+    connect(delay_knob, &knob_integer::changed, this,
+            [this](float value) { data.insert("auto_save_delay", value); });
+
     enable_knob->set_init_space(init_space);
     delay_knob->set_init_space(init_space);
     delay_knob->set_init_label_text("Delay: Seconds");
 
     layout->addWidget(enable_knob);
     layout->addWidget(delay_knob);
+
+    auto_save_enable_pointer = enable_knob;
+    auto_save_delay_pointer = delay_knob;
 }
 
 void general_settings::setup_plugins()
@@ -51,5 +62,21 @@ void general_settings::setup_appearance()
 
 void general_settings::save_settings()
 {
-    print("save");
+    jwrite(VINACOMP_CONF_PATH + "/settings.json", data);
+}
+
+void general_settings::showEvent(QShowEvent *event)
+{
+    restore_settings();
+}
+
+void general_settings::restore_settings()
+{
+    data = jread(VINACOMP_CONF_PATH + "/settings.json");
+
+    static_cast<knob_check_box *>(auto_save_enable_pointer)
+        ->set_check(data.value("auto_save_enable").toBool());
+
+    static_cast<knob_integer *>(auto_save_delay_pointer)
+        ->set_value(data.value("auto_save_delay").toDouble());
 }
